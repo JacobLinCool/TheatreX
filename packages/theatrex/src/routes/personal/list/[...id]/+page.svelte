43ad9ce5ea9d1f2418771d16cec13cd6ea8c0d5d
JacobLinCool <jacoblincool@gmail.com>
2023-01-16 03:37:19 +0800

    feat(client): import list

--- packages/theatrex/src/routes/personal/list/[...id]/+page.svelte
+++ packages/theatrex/src/routes/personal/list/[...id]/+page.svelte
@@ -3,10 +3,10 @@
 	import { page } from "$app/stores";
 	import CreateList from "$lib/CreateList.svelte";
 	import Grid from "$lib/Grid.svelte";
-	import ListItem from "$lib/ListItem.svelte";
+	import UIListItem from "$lib/ListItem.svelte";
 	import yaml from "js-yaml";
 	import Icon from "@iconify/svelte";
-	import type { List } from "@theatrex/types";
+	import type { List, ListItem } from "@theatrex/types";
 
 	export let data: List | { lists: [string, string][] };
 	let list: List = data as List;
@@ -61,11 +61,12 @@
 		update_list(list);
 	}
 
-	function export_list() {
-		const data = yaml.dump(
-			{ name: list.name, items: list.items.map((item) => item.name) },
-			{ indent: 4 },
-		);
+	async function export_list(id: string) {
+		const res = await fetch("/api/personal/list/" + id);
+		const list = (await res.json()) as List;
+		console.log(list);
+
+		const data = yaml.dump({ id: list.id, name: list.name, items: list.items }, { indent: 4 });
 		const blob = new Blob([data], { type: "text/yaml" });
 		const url = URL.createObjectURL(blob);
 		const a = document.createElement("a");
@@ -77,6 +78,22 @@
 		URL.revokeObjectURL(url);
 		a.remove();
 	}
+
+	let imported_name = "";
+	let imported_items: ListItem[] = [];
+	async function import_list(evt: Event) {
+		const input = evt.target as HTMLInputElement;
+		if (!input.files) {
+			return;
+		}
+		const file = input.files[0];
+		const data = await file.text();
+		const list = yaml.load(data) as List;
+		imported_name = list.name;
+		imported_items = list.items;
+
+		document.querySelector<HTMLElement>("#import-new-list")?.click();
+	}
 </script>
 
 <svelte:head>
@@ -86,7 +103,32 @@
 
 <section class="h-full w-full p-2">
 	{#if data && "lists" in data}
-		<h1 class="mb-4 px-4 pt-4 text-2xl font-bold">Personal Lists</h1>
+		<div class="flex w-full items-center justify-between">
+			<h1 class="mb-4 inline-block px-4 pt-4 text-2xl font-bold">Personal Lists</h1>
+
+			<div>
+				<label class="btn btn-outline btn-sm m-2 normal-case" for="import-list">
+					<Icon icon="ant-design:import-outlined" class="inline" />
+				</label>
+				<input
+					type="file"
+					id="import-list"
+					class="hidden"
+					accept=".yaml,.yml"
+					on:change={import_list}
+				/>
+
+				<CreateList name={imported_name} items={imported_items}>
+					<span id="import-new-list" class="hidden" />
+				</CreateList>
+
+				<CreateList>
+					<span class="btn btn-outline btn-sm m-2 normal-case">
+						<Icon icon="ant-design:plus-outlined" class="inline" />
+					</span>
+				</CreateList>
+			</div>
+		</div>
 		{#each data.lists as list}
 			<div class="my-1 flex w-full p-2">
 				<a href="/personal/list/{list[0]}" class="btn mr-2 flex-1 justify-start">
@@ -123,7 +165,10 @@
 					}}
 				/>
 
-				<button class="btn btn-outline btn-sm m-4" on:click={export_list}>
+				<button
+					class="btn btn-outline btn-sm m-4"
+					on:click={() => export_list($page.params.id)}
+				>
 					<Icon icon="mdi:download" />
 				</button>
 			</div>
@@ -137,7 +182,7 @@
 				on:dragover={(evt) => evt.preventDefault()}
 				on:drop|preventDefault={(evt) => drop(evt, idx)}
 			>
-				<ListItem {item} />
+				<UIListItem {item} />
 				<button
 					class="btn btn-xs btn-error absolute top-0 right-0 mx-2 hidden group-hover:block"
 					on:click={() => {
